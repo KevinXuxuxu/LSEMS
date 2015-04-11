2d9475db098097ab406bca975ace68224a0bbe1a
Administrator <admin@example.com>
2015-04-11 03:23:17 +0000

    data management module

---
+++ data.py
@@ -0,0 +1,16 @@
+# use UTF-8 #
+"""
+    THis script is to manage experiment data,
+    including record infos for new data pack,
+    fetching data for experiments, etc.
+"""
+
+__author__ = "Xu Fangzhou"
+__email__ = "kevin.xu.fangzhou@gmail.com"
+
+from json import *
+from pymongo import *
+import csv
+import os
+
+
