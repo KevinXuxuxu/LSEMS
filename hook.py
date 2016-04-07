f215376280da1ef827a6e59ebbb1152fc0fe0101
KevinXuxuxu <kevin.xu.fangzhou@gmail.com>
2016-04-07 10:24:07 +0800

    changed config file position

--- hook.py
+++ hook.py
@@ -76,5 +76,6 @@ class MyApplication(web.application):
 
 if __name__ == "__main__":
     app = MyApplication(urls, globals())
-    config = json.load(open(os.environ.get("HOME") + '/sandbox/config.json'))
+    # config = json.load(open(os.environ.get("HOME") + '/sandbox/config.json'))
+    config = json.load(open("config.json"))
     app.run(port = config['hook_port'])
